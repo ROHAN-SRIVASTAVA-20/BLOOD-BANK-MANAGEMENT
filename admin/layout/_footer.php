35bf6f692ae7c83c228a35918f74b74a4238b9c7
ROHAN SRIVASTAVA <72071526+ROHAN-SRIVASTAVA-20@users.noreply.github.com>
2022-04-14 17:27:32 +0530

    Add files via upload

---
+++ admin/layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="nav navbar-fixed-bottom">
+            <b>Made By: GROUP 1</a></b>
+            <br>
+            &COPY;GROUP1
+</footer>
+
+
+
+</body>
+</html>
---
+++ layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="foter">
+            <b>Made By: GROUP 1 </a></b>
+            <br>
+            &COPY;GROUP1
+</footer>
+
+
+
+</body>
+</html>
